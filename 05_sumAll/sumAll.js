c21b08204e84fd0ebea00ac9e13bfb901cefa284
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-20 17:38:07 +0000

    Sum with large number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,15 @@
 const sumAll = function(start, finish) {
     let sum = 0;
-    for (i = start; i <= finish; i++) {
-        sum += i;
+    if (start < finish) {
+        for (i = start; i <= finish; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = start; i >= finish; i--) {
+            sum += i;
+        }
     }
+
     return sum;
 };
 
