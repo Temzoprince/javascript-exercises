03a90099223ec1d83bd5280694e66018ef50d90a
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-20 17:02:42 +0000

    Pass two tests
    Multiple value are removed from array
    Rest parameters are used - function passes each value through array to
    see if it exists and removes it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,14 @@
-const removeFromArray = function(array, value) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === value) {
-            array.splice(i, 1);
+const removeFromArray = function(array, ...valueArgs) {
+    // check if each value is in the array and remove it
+    outer:
+    for (value of valueArgs) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === value) {
+                array.splice(i, 1);
+            }
         }
     }
+    
     return array;
 };
 
