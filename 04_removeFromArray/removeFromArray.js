ff17f947310af558b60d6ff1d48cff957d8178bd
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-03-20 17:17:51 +0000

    Pass 3 tests
    Removes multiple of same value
    Previous commit did not work because I did not account for change in
    index and reduction in array length after splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,18 @@
 const removeFromArray = function(array, ...valueArgs) {
     // check if each value is in the array and remove it
-    outer:
     for (value of valueArgs) {
         for (let i = 0; i < array.length; i++) {
             if (array[i] === value) {
                 array.splice(i, 1);
+                i--;
             }
         }
     }
     
     return array;
 };
+
+console.log(removeFromArray([1,2,2,3], 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
