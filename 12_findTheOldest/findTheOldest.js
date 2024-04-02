5133930330276f4bac8a680c7e572d7d0eeef762
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-04-02 21:27:20 +0100

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    oldest = array.sort((a,b) => {
+        if (!a.yearOfDeath) {
+            ageA = (new Date().getFullYear()) - a.yearOfBirth
+        } else {
+            ageA = a.yearOfDeath - a.yearOfBirth
+        }
 
+        if (!b.yearOfDeath) {
+            ageB = (new Date().getFullYear()) - b.yearOfBirth
+        } else {
+            ageB = b.yearOfDeath - b.yearOfBirth
+        }
+        
+        if (ageA > ageB) {
+            return -1
+        } else {
+            return 1
+        }
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
