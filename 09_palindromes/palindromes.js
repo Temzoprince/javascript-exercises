3555c5aff7c192c6aac53c2a34a4686bf6d3580e
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-04-02 17:22:58 +0100

    Pass tests two pointer method

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    newStr = str
+                .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')
+                .toLowerCase()
+                .split(" ")
+                .join("")
+
+    for (l = 0, r = newStr.length-1; l < newStr.length, r > 0; l++, r--) {
+        if (newStr.charAt(l) !== newStr.charAt(r)) {
+            return false;
+        }
+    }
+    return true;
 };
+
+palindromes('racecar')
 
 // Do not edit below this line
 module.exports = palindromes;
